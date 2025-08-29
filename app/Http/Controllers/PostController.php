0260f89df7189a775ef8517e684e1c1b0e015868
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-08-28 20:21:30 -0500

    feat: :art: Add Post model, factory, and seeder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Traits\ApiResponse;
+
+//TODO: Eliminar
+use Illuminate\Support\Facades\DB;
+
 
 class PostController extends Controller
 {
     use ApiResponse;
-
-    public function __invoke()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return $this->ok("Desde invoke");    
+        // La mala practica porque tenemos un Model
+        // return response()->json(DB::table('posts')->get());
+        return $this->ok("Todo ok, como dijo el Pibe", Post::get());
     }
-}
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $newPost = new Post();
+        $newPost->fill($request->only(["" , ""]));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $result = Post::find($id);
+        if ($result) {
+            return $this->ok("Todo ok, como dijo el Pibe", $result);
+        } else {
+            return $this->success("Todo mal, como NO dijo el Pibe", [], 404);
+    }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+} 
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Crear inserts o datos Mocks con Factory
+        Post::factory()->count(50)->create();
+        //Crear un solo datos con random haciendo uso de Insert
+        DB::table('posts')->insert([
+            'title' => Str::random(10),
+            'content' => Str::random(10).'@example.com',
+            'status' => true,
+        ]);
+    }
+}
